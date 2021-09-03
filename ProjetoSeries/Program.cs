224208ceef87fabdfd55c4d904c2739d0c752f54
Winicius Cabral <winicius.cabral@hotmail.com>
2021-09-03 15:20:49 -0300

    Update Program.cs
    Implementando os métodos no programa principal

--- ProjetoSeries/Program.cs
+++ ProjetoSeries/Program.cs
@@ -4,9 +4,106 @@ namespace ProjetoSeries
 {
     class Program
     {
+        static SerieRepo repo = new SerieRepo();
         static void Main(string[] args)
         {
-          
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries");
+
+            var lista = repo.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série");
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o genêro entre as opções acima: ");
+            int includeGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o título da série: ");
+            string includeTitulo = Console.ReadLine();
+
+            Console.Write("Digite o ano de lançamento da série: ");
+            int includeAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a descrição da série: ");
+            string includeDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repo.ProximoId(), genero: (Genero)includeGenero,
+            titulo: includeTitulo,
+            ano: includeAno,
+            descricao: includeDescricao);
+
+            repo.Insere(novaSerie);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Play Séries a seu dispor!!");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1- Listar séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
